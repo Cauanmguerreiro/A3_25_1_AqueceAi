804e629c753e2ad8dd221d47fbd21e91c4e480a8
Cauanmguerreiro <cauanguerreiro@hotmail.com>
2025-06-11 20:39:55 -0300

    Alterei MainLayout Para facilitar a visualização e estruturação

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh lpR lFf">
+
     <q-header elevated>
       <q-toolbar>
         <q-btn
@@ -17,7 +18,16 @@
           </div>
         </header>
 
-        
+        <q-space />
+
+        <q-btn
+          flat
+          dense
+          round
+          icon="account_circle"
+          aria-label="Conta"
+          @click="toggleRightDrawer"
+        />
       </q-toolbar>
     </q-header>
 
@@ -27,12 +37,24 @@
       bordered
     >
       <q-list>
-        <q-item-label
-          header
+        <q-item-label header>Menu</q-item-label>
+        <q-item
+          v-for="item in navigationLinks"
+          :key="item.title"
+          clickable
+          v-ripple
+          :to="item.to"
+          exact
         >
-          Nossos GitHub's
-        </q-item-label>
+          <q-item-section avatar>
+            <q-icon :name="item.icon" />
+          </q-item-section>
+          <q-item-section>{{ item.title }}</q-item-section>
+        </q-item>
 
+        <q-separator class="q-my-md" />
+
+        <q-item-label header>Nossos GitHub's</q-item-label>
         <EssentialLink
           v-for="link in linksList"
           :key="link.title"
@@ -41,74 +63,70 @@
       </q-list>
     </q-drawer>
 
+    <q-drawer
+      v-model="rightDrawerOpen"
+      side="right"
+      overlay
+      bordered
+    >
+      <q-list>
+        <q-item-label header>Menu Usuário</q-item-label>
+        <q-item clickable v-ripple>
+          <q-item-section avatar><q-icon name="person" /></q-item-section>
+          <q-item-section>Meu Perfil</q-item-section>
+        </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section avatar><q-icon name="settings" /></q-item-section>
+          <q-item-section>Configurações</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <q-footer elevated class="primary">
+    <q-footer elevated class="$primary">
       <q-toolbar>
         <q-toolbar-title>
           <div class="text-caption">© {{ new Date().getFullYear() }} Aquece Aí. Todos os direitos reservados.</div>
         </q-toolbar-title>
-
-        <div>
-          <q-btn flat round dense icon="code" type="a" href="https://github.com/Cauanmguerreiro/A3_25_1_AqueceAi" target="_blank" />
-        </div>
       </q-toolbar>
     </q-footer>
 
   </q-layout>
 </template>
 
-<script>
-import { defineComponent, } from 'vue'
+<script setup>
+import { ref } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
 
-const linksList = [
-  {
-    title: 'Github do Cauan',
-    caption: 'github.com/Cauanmguerreiro',
-    icon: 'code',
-    link: 'https://github.com/Cauanmguerreiro'
-  },
-  {
-    title: 'Github do César',
-    caption: 'github.com/Bugmaer',
-    icon: 'code',
-    link: 'https://github.com/Bugmaer'
-  },
-  {
-    title: 'Github Guilherme',
-    caption: 'github.com/Cauanmguerreiro',
-    icon: 'code',
-    link: 'https://github.com/Cauanmguerreiro'
-  },
-  {
-    title: 'Github Pedro',
-    caption: 'github.com/Cauanmguerreiro',
-    icon: 'code',
-    link: 'https://github.com/Cauanmguerreiro'
-  },
+// Lista para links de navegação interna
+const navigationLinks = [
+  { title: 'Página Inicial', icon: 'home', to: '/' },
+  { title: 'Sobre', icon: 'info', to: '/sobre' },
+  { title: 'Contatos', icon: 'contacts', to: '/contatos' },
+  { title: 'Localizações', icon: 'location_on', to: '/endereco' }
 ]
 
-export default defineComponent({
-  name: 'MainLayout',
+// Lista para links externos (GitHub)
+const linksList = [
+  // Lembre-se de preencher com os dados corretos
+  { title: 'Github do Cauan Guerreiro', caption: 'github.com/Cauanmguerreiro', icon: 'code', link: 'https://github.com/Cauanmguerreiro' },
+  { title: 'Github do César Bugmaer', caption: 'github.com/Bugmaer', icon: 'code', link: 'https://github.com/Bugmaer' },
+  { title: 'Github do Guilherme Serrano', caption: 'github.com/guiserranodev1', icon: 'code', link: 'https://github.com/guiserranodev1' },
+  { title: 'Github do Pedro Paz', caption: 'github.com/pedropazs', icon: 'code', link: 'https://github.com/pedropazs' },
+]
 
-  components: {
-    EssentialLink
-  },
+// Variáveis reativas para controlar o estado dos menus
+const leftDrawerOpen = ref(false)
+const rightDrawerOpen = ref(false)
 
-  data () {
-    return {
-      linksList,
-      leftDrawerOpen: false
-    }
-  },
-
-  methods: {
-    toggleLeftDrawer () {
-      this.leftDrawerOpen = !this.leftDrawerOpen
-    }
-  }
-})
+// Funções para alterar o estado (abrir/fechar os menus)
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+function toggleRightDrawer () {
+  rightDrawerOpen.value = !rightDrawerOpen.value
+}
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'sobre', component: () => import('pages/Sobre.vue') }, 
+      { path: 'sobre', component: () => import('pages/SobrePage.vue') },
       { path: 'contatos', component: () => import('pages/Contatos.vue') }, 
       { path: 'endereco', component: () => import('pages/Localizacao.vue') }  
       
